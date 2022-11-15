0d0d55c4b0820fb8603ac031dd0a225ffcca3c7e
caio-ggomes <caio.graca@gmail.com>
2022-11-14 22:54:50 -0300

    implemented raymond

--- MEAlgorithms/Raymond/Process.go
+++ MEAlgorithms/Raymond/Process.go
@@ -12,19 +12,21 @@ import (
 
 // Variáveis globais interessantes para o processo
 var err string
-var myPort string                         // Porta do meu servidor
-var nServers int                          // Quantidade de outros processos
-var CliAddr map[int]*net.UDPAddr          // Dicionário com endereços das conexões para os servidores dos outros processos
-var revCliAddr map[*net.UDPAddr]int       // Dicionário com endereços das conexões para os servidores dos outros processos
-var CliConn map[*net.UDPAddr]*net.UDPConn // Dicionário com conexões para os servidores dos outros processos
-var CoordAddr *net.UDPAddr                // Endereço da conexão com o coordenador
-var CoordConn *net.UDPConn                // Conexão para o servidor do coordenador
+var myPort string // Porta do meu servidor
 
-var myId int          // Identidade deste processo
-var requested bool    // Requisitou token
-var coordinatorId int // Identidade do processo coordenador
-var myToken bool      // Coordenador tem ou não o token atualmente
-var myQueue []int     // Fila de processos que pediram Request ao coordenador
+var myId int // Identidade deste processo
+
+var leftConn *net.UDPConn
+var rightConn *net.UDPConn
+var parentConn *net.UDPConn
+
+var addrMap map[int]*net.UDPAddr
+var revAddrMap map[*net.UDPAddr]int
+var connMap map[*net.UDPAddr]*net.UDPConn
+
+var forwardRequest bool
+var holder int
+var myQueue []int // Fila de processos que pediram Request ao coordenador
 
 var mutexTokenQueue sync.Mutex // Mutex para garantir correta manipulação da variável Fila
 var mutexRequest sync.Mutex    // Mutex para garantir correta manipulação da variável request
@@ -50,9 +52,7 @@ func CheckError(err error) {
 
 // Rotina para acessar o recurso compartilhado
 func accessSharedResource() {
-	mutexRequest.Lock()
 	fmt.Println("Entrei na CS")
-	time.Sleep(time.Second * 1)
 	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1"+":10001")
 	CheckError(err)
 	Conn, err := net.DialUDP("udp", nil, ServerAddr)
@@ -60,52 +60,49 @@ func accessSharedResource() {
 	// Enviar mensagem com ID e TIMESTAMP, além de um texto básico
 	_, err = Conn.Write([]byte("ID: " + strconv.Itoa(myId) + "\nACESSEI A CS\n"))
 	CheckError(err)
+	time.Sleep(time.Second * 1)
 	Conn.Close()
 	fmt.Println("Saí da CS")
-	go doClientJob(CoordAddr, "RELEASE TOKEN")
-	requested = false
-	mutexRequest.Unlock()
 }
 
 func doServerJob() {
 
 	buf := make([]byte, 1024)
 	// Ler da conexão UDP a mensagem no buffer e tomar as devidas medidas em caso de receber REQUEST ou RELEASE
-
-	if myId == coordinatorId {
-		for {
-			n, addr, err := ServerConn.ReadFromUDP(buf)
-			CheckError(err)
-			msg := string(buf[0:n])
-			fmt.Println("Received\n"+msg+"from", addr)
-			if msg[0:13] == "REQUEST TOKEN" {
-				mutexTokenQueue.Lock()
-				if myToken {
-					myToken = false
-					go doClientJob(addr, msg)
-				} else {
-					myQueue = append(myQueue, revCliAddr[addr])
-				}
-				mutexTokenQueue.Unlock()
-			} else if msg[0:13] == "RELEASE TOKEN" {
-				myToken = true
-				if len(myQueue) > 0 {
-					mutexTokenQueue.Lock()
-					myToken = false
-					go doClientJob(CliAddr[myQueue[0]], "GRANT TOKEN")
-					myQueue = myQueue[1:]
-					mutexTokenQueue.Unlock()
+	for {
+		n, addr, err := ServerConn.ReadFromUDP(buf)
+		CheckError(err)
+		msg := string(buf[0:n])
+		fmt.Println("Received\n"+msg+"from", addr)
+		if msg[0:7] == "REQUEST" {
+			if holder == myId {
+				holder = revAddrMap[addr]
+				go doClientJob(addrMap[holder], "TOKEN")
+			} else {
+				myQueue = append(myQueue, revAddrMap[addr])
+				if !forwardRequest {
+					forwardRequest = true
+					go doClientJob(addrMap[holder], "REQUEST")
 				}
 			}
-		}
-	} else {
-		for {
-			n, addr, err := ServerConn.ReadFromUDP(buf)
-			CheckError(err)
-			msg := string(buf[0:n])
-			fmt.Println("Received\n"+msg+"from", addr)
-			if msg[0:11] == "GRANT TOKEN" {
+		} else if msg[0:5] == "TOKEN" {
+			entry := myQueue[0]
+			myQueue = myQueue[1:]
+			if entry == myId {
 				go accessSharedResource()
+				if len(myQueue) > 0 {
+					holder = myQueue[0]
+					myQueue = myQueue[1:]
+					go doClientJob(addrMap[holder], "TOKEN")
+					if len(myQueue) > 0 {
+						go doClientJob(addrMap[holder], "REQUEST")
+					}
+				}
+			} else {
+				go doClientJob(addrMap[entry], "TOKEN")
+				if len(myQueue) > 0 {
+					go doClientJob(addrMap[holder], "REQUEST")
+				}
 			}
 		}
 	}
@@ -115,16 +112,12 @@ func doServerJob() {
 func doClientJob(otherProcessAddress *net.UDPAddr, msg string) {
 	buf := make([]byte, 1024)
 	buf = []byte(msg)
-	_, err := CliConn[otherProcessAddress].Write(buf)
+	_, err := connMap[otherProcessAddress].Write(buf)
 	CheckError(err)
 }
 
 // Método para iniciar as conexões entre todos os processos
 func initConnections() {
-
-	nServers = (len(os.Args) - 4) / 2
-	/*Esse -4 tira o nome (no caso ./Process), o 'c' ou 'p', o Id deste processo, e a porta
-	deste processo. As demais portas são dos outros processos*/
 
 	//CliConn = make([]*net.UDPConn, nServers)
 
@@ -133,55 +126,37 @@ func initConnections() {
 	ServerConn, err = net.ListenUDP("udp", ServerAddr)
 	CheckError(err)
 
-	if myId == coordinatorId {
-		/*Conexões com os servidores de cada processo. Colocar tais conexões no vetor CliConn.*/
-		for process := 0; process < nServers; process++ {
-			ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1"+os.Args[2*process+5])
+	for i := 0; i < 3; i++ {
+		if os.Args[2*i+3] != "NULL" {
+			id, err := strconv.Atoi(os.Args[3])
 			CheckError(err)
-			id, err := strconv.Atoi(os.Args[2*process+4])
+			ServerAddr, err = net.ResolveUDPAddr("udp", "127.0.0.1"+os.Args[2*i+4])
 			CheckError(err)
-			CliAddr[id] = ServerAddr
-			revCliAddr[ServerAddr] = id
+			addrMap[id] = ServerAddr
+			revAddrMap[ServerAddr] = id
 			Conn, err := net.DialUDP("udp", nil, ServerAddr)
 			CheckError(err)
-			CliConn[ServerAddr] = Conn
+			connMap[ServerAddr] = Conn
 		}
-	} else {
-		coordinatorPort := os.Args[6]
-		ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1"+coordinatorPort)
-		CoordAddr = ServerAddr
-		CheckError(err)
-		Conn, err := net.DialUDP("udp", nil, CoordAddr)
-		CheckError(err)
-		CoordConn = Conn
 	}
 }
 
-// A entrada para o coordenador será da forma ./Process c {myId} {myPort} {Process_1_id} {Process_1_port} ... {Process_n_id} {Process_n_port}
-// Já para o processo, a entrada será ./Process p {myId} {myPort} {Coordinator_id} {Coordinator_port}
+// A entrada para o coordenador será da forma ./Process {myId} {myPort} {parent_id} {parent_port} {left_id} {left_port} {right_id} {right_port}
 func main() {
 
 	// Inicialização das variáveis
-	id, err := strconv.Atoi(os.Args[2])
+	id, err := strconv.Atoi(os.Args[1])
 	CheckError(err)
 	myId = id
-	myPort = os.Args[3]
-	myQueue = make([]int, 0)
-
-	if os.Args[1] == "c" {
-		coordinatorId = myId
-	} else {
-		id, err := strconv.Atoi(os.Args[4])
-		CheckError(err)
-		coordinatorId = id
-	}
+	myPort = os.Args[2]
+	myQueue = make([]int, 3)
 
 	// Iniciar conexões
 	initConnections()
 
 	// O fechamento de conexões ocorrerá quando a main retornar
 	defer ServerConn.Close()
-	for _, conn := range CliConn {
+	for _, conn := range connMap {
 		defer conn.Close()
 	}
 
@@ -190,37 +165,34 @@ func main() {
 
 	// Escutar outros processos
 	go doServerJob()
-	if myId != coordinatorId {
-		for {
-			// Verificar (de forma não bloqueante) se tem algo no stdin (input do terminal)
-			select {
-			case x, valid := <-ch:
-				if valid {
-					fmt.Printf("Recebi do teclado: %s \n", x)
-					// Se a mensagem for "x", então pediu acesso à CS
-					if x == "x" {
-						// Pedido indevido
-						if requested {
-							fmt.Println("x ignorado")
-						} else {
-							mutexRequest.Lock()
-							requested = true
-							mutexRequest.Unlock()
-							go doClientJob(CoordAddr, "REQUEST TOKEN")
-						}
-					}
-				} else {
-					fmt.Println("Canal fechado!")
-				}
-			default:
-				// Fazer nada!
-				// Mas não fica bloqueado esperando o teclado
-				time.Sleep(time.Millisecond * 400)
-			}
 
-			// Espera um pouco
+	for {
+		// Verificar (de forma não bloqueante) se tem algo no stdin (input do terminal)
+		select {
+		case x, valid := <-ch:
+			if valid {
+				fmt.Printf("Recebi do teclado: %s \n", x)
+				// Se a mensagem for "x", então pediu acesso à CS
+				if x == "x" {
+					// Pedido indevido
+					if len(myQueue) == 0 && myId != holder {
+						go doClientJob(addrMap[holder], "REQUEST")
+						myQueue = append(myQueue, myId)
+					} else {
+						fmt.Println("x ignorado")
+					}
+				}
+			} else {
+				fmt.Println("Canal fechado!")
+			}
+		default:
+			// Fazer nada!
+			// Mas não fica bloqueado esperando o teclado
 			time.Sleep(time.Millisecond * 400)
 		}
+
+		// Espera um pouco
+		time.Sleep(time.Millisecond * 400)
 	}
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Análise comparativa entre algoritmos de exclusão mútua
 
-## Laboratório de Exame-CES27
-## Grupo: Caio Graça Gomes e Thomas Alberto de Castro Neto
+Laboratório de Exame-CES27
+
+## Contribuidores
+
+| [<img src="https://avatars.githubusercontent.com/u/54087165?v=4" width="115"><br><sub>@caio-ggomes</sub>](https://github.com/caio-ggomes) | [<img src="https://avatars.githubusercontent.com/u/80851723?v=4" width="115"><br><sub>@thomascastroneto</sub>](https://github.com/thomascastroneto) |
+|:-:|:-:|:-:|
+
+Comandos para execução
+
+No diretório SharedResource
+```{c}
+$ go build SharedResource.go
+./SharedResource
+```
+
+Nos demais diretórios dentro de MEAlgorithms
+```{c}
+$ go build Process.go
+./Process
+```
